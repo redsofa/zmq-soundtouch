f77405f2286bcb162b8718044288e99fe45c05b5
Rene Richard <rene@redsofa.ca>
2016-02-11 21:06:54 -0400

    initial version of visualization dot colors

--- pull-zmq-event-collector-ws/main/webroot/js/app.js
+++ pull-zmq-event-collector-ws/main/webroot/js/app.js
@@ -1,185 +1,14 @@
-/**************************************************************** 
-*  Very simple graph storage class for illustration purposes 
-*/
-function GraphStore(){
-  this.nodes = Array();
-}
-
-GraphStore.prototype.addNode = function (id, iNodeType) {
-  this.nodes.push({"id":id, "nodeType":iNodeType});
-};
-
-
-GraphStore.prototype.getNodes = function(){
-  return this.nodes;
-};
-
-/****************************************************************/
-
-
-/****************************************************************/
-//Basic Force directed graph class for illustration purposes
-function Vis(iSelector){
-  this.w = 500;
-  this.h = 500;
-  this.selector = iSelector;
-  this.graphStore = null;
-  this.svg = null;
-  this.force = null;
-}
-
-Vis.prototype.createSVG = function(){
-  var retVal = d3.select(this.selector)
-    .append("svg")
-    .attr("width", this.w)
-    .attr("height", this.h)
-    .attr("border", 1)
-    .attr("id", "svg");
-
-   retVal.append("svg:g");
-
-   var borderPath = retVal.append("rect")
-               .attr("x", 0)
-               .attr("y", 0)
-               .attr("height", this.h)
-               .attr("width", this.w)
-               .style("stroke", "black")
-               .style("fill", "none")
-               .style("stroke-width", 1);
-
-    return retVal;
-
-};
-
-Vis.prototype.createForceLayout = function(){
-  return d3.layout.force()
-    .size([this.w, this.h])
-    .gravity(.05)
-    .distance(50)
-    .nodes(this.graphStore.getNodes());
-};
-
-Vis.prototype.setGraphStorage = function(iStorage){
-  this.graphStore = iStorage;
-};
-
-Vis.prototype.recreateContainer = function(){
-  d3.select('#svg').remove();
-  this.svg = this.createSVG();
-  this.force = this.createForceLayout();
-}
-
-Vis.prototype.update =function(){ 
-  var sRef = this;
-
-  this.recreateContainer();
-
-
-  var forceDragBehavior = function(){
-    var retVal = sRef.force.drag()
-      .on("dragstart", function(d){
-        
-      })
-      .on("drag", function(d){
-           
-      })
-      .on("dragend", function(d){
-
-      });
-    return retVal;
-  };
-
-
-  var createNodes = function(){
-    var node = sRef.svg.append("g")
-      .selectAll("circle.node")
-      .data(sRef.graphStore.getNodes(), function(d) { 
-          return d.id;
-      });
-
-    var nodeEnter = node.enter()
-      .append("svg:circle")
-      .attr("class", "node")
-      .attr('cursor', 'move')
-      .attr("cx", function(d) { return d.x; })
-      .attr("cy", function(d) { return d.y; })
-      .attr("fill", function(d){        
-        if (d.nodeType == "VolChange"){
-          return "lightBlue";
-        }else if(d.nodeType == "Heartbeat"){
-          return "red" ;
-        }else if(d.nodeType == "PresetChange"){
-          return "green" ;
-        }else if(d.nodeType == "SocketOpen"){
-          return "orange" ;
-        }else if(d.nodeType == "SocketClose"){
-          return "blue" ;      
-        }else if(d.nodeType == "RecentsUpdate"){
-          return "grey" ;
-        }else{
-          return "black"
-        }
-      })
-      .style("stroke", "black")      
-      .style("stroke-width", 1)
-      .style("opacity", 0.7)
-      .attr("r", 10)
-      .attr("id",function(d) { 
-        return "Node;" + d.id;
-      })
-      .call(forceDragBehavior());
-
-    node.exit().remove();
-
-    return node;
-  };
-
-  var onForceNodeTick = function(e){
-    //When you fire tick, the previous positions of nodes are used to determine 
-    //the new positions, and are stored in the px and py attributes. 
-    //If you change these attributes, running tick will update 
-    //the x and y attributes to these values.    
-    node.attr("cx", function(d) { return d.x; })
-        .attr("cy", function(d) { return d.y; });
-  };
-
-  this.force.on("tick", function(e) {
-      onForceNodeTick(e);
-  });
-
-  var node = createNodes();
-
-
-  function setAllNodesFixed(){
-    sRef.graphStore.getNodes().forEach(function (e){
-        e.fixed = true;
-    });
-  };
-
-  this.startForceLayout(500);
-  setAllNodesFixed();
-};
-
-Vis.prototype.startForceLayout = function(iIterationCount){
-    this.force.start();
-    var n = iIterationCount;
-    for (var i = n * n; i > 0; --i) {
-      this.force.tick();
-    }
-    this.force.stop();
-}
-
 
 
 $( document ).ready(function($) {
    var eventLogCounter = 0;
 
-   function log(iMessage){
-      eventLogCounter++ ;
+   function log(iMessage, iClass){      
       if (eventLogCounter % 10 === 0){
          $("#log").empty();
       }
-      $("#log").append("<p>" + eventLogCounter + " - " + iMessage + "</p>")
+      $("#log").append('<table><tr><td><div class="'+ iClass + '">&nbsp;&nbsp;&nbsp;</div></td><div><td> &nbsp;&nbsp;' + eventLogCounter + ' - ' + iMessage + '</td></tr></table></div>');
+      eventLogCounter++ ;
    }
 
    function connectWebSocket(){
@@ -188,7 +17,7 @@ $( document ).ready(function($) {
          var ws = new WebSocket("ws://localhost:8081/entry");
             
          ws.onopen = function(){
-            log("Opened Socket Connection");
+            log("Opened Socket Connection", "orange");
             addNode("SocketOpen");
          }  
 
@@ -240,24 +69,26 @@ $( document ).ready(function($) {
 
       if ($volumeUpdated.length ){
          $targetVolume = $volumeUpdated.find("targetvolume");
-         log("Volume Change to : " + $targetVolume.text());
+         log("Volume Change to : " + $targetVolume.text(), "lightblue");
          addNode("VolChange");         
       }else if ($presetUpdated.length) {
          $preset = $presetUpdated.find("itemName");          
-         log("Preset Change : " + $preset.text());
+         log("Preset Change : " + $preset.text(), "green");
          addNode("PresetChange");
       }else if ($recentsUpdated.length){       
          $recents = $recentsUpdated.find("recents");
-         log("Recents List Update");
+         log("Recents List Update", "grey");
          addNode("RecentsUpdate");
       }else if ($connectionStatusUpdated.length) {
-         log("Heartbeat from SoundTouch");
+         log("Heartbeat from SoundTouch", "red");
          addNode("Heartbeat");
       }
       //console.log(xmlDoc);
    } 
 
    connectWebSocket();
+
+
 
 });
 
