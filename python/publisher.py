c5d5cba5099f96fc0a0a9459c960f3571732b3bc
Andriy Drozdyuk <Andriy.Drozdyuk@nrc-cnrc.gc.ca>
2016-02-18 16:01:29 -0500

    Enabled secure pull socket by default and option to allow insecure one

--- python/publisher.py
+++ python/publisher.py
@@ -5,17 +5,24 @@ import sys
 import argparse
 import zmq
 import os
-from helpers import insecure_client
+from helpers import insecure_client, secure_client
 
 
-def run(pull_address, pub_address):
+def run(pull_address, pub_address, insecure):
     ctx = zmq.Context.instance()
 
     pub = ctx.socket(zmq.PUB)
     logging.debug("PUB bound to %s" % pub_address)
     pub.bind(pub_address)
 
-    with insecure_client(zmq.PULL) as pull:
+    if insecure:
+        logging.warn("Creating insecure PULL socket.")
+        client = insecure_client
+    else:
+        logging.debug("Creating secure PULL socket.")
+        client = secure_client
+
+    with client(zmq.PULL) as pull:
         logging.debug("PULL connected to %s" % pull_address)
         pull.connect(pull_address)
 
@@ -29,6 +36,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Listens to a PULL socket and publishes to a PUB socket.')
     parser.add_argument('--pull', dest='pull', help='Host to listen on (localhost, port 7000 by default)', default="tcp://127.0.0.1:7000")
     parser.add_argument('--pub', dest='pub', help='Address to publish on (localhost, port 7001 by default)', default="tcp://127.0.0.1:7001")
+    parser.add_argument('--insecure', dest='insecure', help='Run without security (useful for testing)', action='store_const', const=True, default=False)
     parser.add_argument('-v', dest='verbose', help='Verbose mode', action="store_const", const=True)
     args = parser.parse_args()
     
@@ -41,4 +49,4 @@ if __name__ == '__main__':
         level = logging.INFO
 
     logging.basicConfig(level=level, format="[%(levelname)s] %(message)s")
-    run(args.pull, args.pub)
+    run(args.pull, args.pub, args.insecure)
